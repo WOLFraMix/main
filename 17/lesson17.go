8808ba8e7ccfa774bfebc17b7b2781c9f505f178
Framix <bgan-stepan@mail.ru>
2026-06-22 11:45:59 +0400

    stepik

--- 17/lesson17.go
+++ 17/lesson17.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+var (
+	goodsCount = 13
+	numberPair = 14
+	classRoom  int
+)
+
+const (
+	li = 3.0
+	si = 8.1
+	di
+)
+
 func main() {
-	fmt.Println("lets GO")
+	a := 5
+	b := 10
+	fmt.Println("lets GO", a)
+	a = 20
+	fmt.Println("say it", b, a)
+	var age int = 30
+	fmt.Println(age)
+	ace := 50
+	fmt.Println(ace)
+	var x, y int = 1, 2
+	fmt.Println(x, y)
+	z, d := 3, 4
+	fmt.Println(z, d)
+	fmt.Println(goodsCount, numberPair, classRoom)
+	age, ace = 31, 88
+	fmt.Println(age, ace)
+	const pi = 3.14
+	fmt.Println(pi)
+	fmt.Println(li, si, di)
 }
