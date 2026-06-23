98f1cf9a6c2341a685d5beb2940cb85161360b09
Framix <bgan-stepan@mail.ru>
2026-06-23 18:28:29 +0400

    add lesson 18

---
+++ 18/lesson18.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	num1 := 12
+	num2 := 0.5
+	str := "hello"
+	fmt.Printf("Тип переменной num1: %T\n", num1) // Тип переменной num1: int
+	fmt.Printf("Тип переменной num2: %T\n", num2) // Тип переменной num2: float64
+	fmt.Printf("Тип переменной str: %T\n", str)   // Тип переменной str: string
+}
+
+// определение типов переменных
