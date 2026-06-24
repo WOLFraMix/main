5125853dd6186990ea0f1cbbc6260c047d4d5ac4
Framix <bgan-stepan@mail.ru>
2026-06-24 11:05:49 +0400

    add lesson 19

---
+++ 19/lesson19.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// системы счисления
+func main() {
+	num1 := 67
+	fmt.Println(num1)
+
+	num2 := 0b01100010
+	fmt.Println(num2)
+
+	num3 := 02345
+	fmt.Println(num3)
+
+	num4 := 0x7bf2
+	fmt.Println(num4)
+
+}
