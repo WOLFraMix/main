8668b0973363d854d3d88078ebaf3b9ac43da3f6
Framix <bgan-stepan@mail.ru>
2026-08-12 13:08:45 +0400

    add lesson 118

---
+++ 118/lesson118.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// разбираем предложение
+	sentence := "Добро пожаловать"
+
+	// инициализируем map
+	// ключами будут знаки, а значениями — количество вхождений
+	frequency := make(map[rune]int)
+
+	// пройдёмся по знакам в предложении
+	for _, v := range sentence {
+		frequency[v]++ // встреченному знаку увеличиваем частоту
+	}
+
+	// печатаем
+	for k, v := range frequency {
+		fmt.Printf("Знак %c встречается %d раз \n", k, v)
+	}
+}
