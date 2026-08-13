fd6c2bd576744aa4fd4bf6e45cdfc304d3c6c4e4
Framix <bgan-stepan@mail.ru>
2026-08-13 15:04:41 +0400

    add lesson 126

--- 125/lesson125.go
+++ 125/lesson125.go
@@ -3,14 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	str := "Зашифруй меня!"
-	encodedStr := CaesarCode(str, 5, true)
+	str := "Зашифруй меня!"                // шифр
+	encodedStr := CaesarCode(str, 5, true) // зашифровка
 	fmt.Println(encodedStr)
 
-	decodedStr := CaesarCode(encodedStr, 5, false)
+	decodedStr := CaesarCode(encodedStr, 5, false) // дешифровка
 	fmt.Println(decodedStr)
 }
 
+// функция которая шифрует и дешифрует текст по алгоритму шифра Цезаря
 func CaesarCode(text string, shift int, encode bool) string {
+	// text - текст, который нужно зашифровать или расшифровать
+	// shift - число, на которое нужно сдвинуть символы
+	// encode - нужно зашифровать (true) или дешифровать (false) сообщение
+	var result string
 
+	if encode == true {
+		for _, v := range text {
+			v += rune(shift)
+			result += string(v)
+		}
+	}
+
+	if encode == false {
+		for _, v := range text {
+			v -= rune(shift)
+			result += string(v)
+		}
+	}
+
+	return result
 }
