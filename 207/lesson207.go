64809231c4604633480cbb7358fff58d9dc6ed11
Framix <bgan-stepan@mail.ru>
2026-09-03 10:54:06 +0400

    add lesson 207

---
+++ 207/lesson207.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1<<20)
+	writer := bufio.NewWriterSize(os.Stdin, 1<<20)
+	defer writer.Flush()
+
+	readInt := func() (int, error) {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return 0, err
+		}
+		line = strings.TrimSpace(line)
+		v, err := strconv.Atoi(line)
+		if err != nil {
+			return 0, err
+		}
+		return v, nil
+	}
+
+	m, _ := readInt()
+	n, _ := readInt()
+
+	result := m + n
+
+	writer.WriteString(strconv.Itoa(result))
+	writer.WriteByte('\n')
+}
