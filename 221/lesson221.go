ba5d334ad93f617b888b07e8b12d6153c8f00980
Framix <bgan-stepan@mail.ru>
2026-09-05 16:57:08 +0400

    add lesson 221

---
+++ 221/lesson221.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+var (
+	ErrSimple = errors.New("simple error")
+)
+
+func main() {
+	if err := fn1(); err != nil {
+		if errors.Is(err, ErrSimple) {
+			log.Fatalf("simple error occurred")
+		} else {
+			log.Fatalf("unknown error")
+		}
+	}
+}
+
+func fn1() error {
+	return fmt.Errorf("fn2: %w", fn2())
+}
+
+func fn2() error {
+	return ErrSimple
+}
